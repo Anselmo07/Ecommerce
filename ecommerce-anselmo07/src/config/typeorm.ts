ff64eb0b2cd789a03bc17831cf5ffcbe3e3fbfa6
anselmo <anselmo.vecchio.jose@gmail.com>
2024-12-18 19:17:27 -0300

    prueba 6

--- ecommerce-anselmo07/src/config/typeorm.ts
+++ ecommerce-anselmo07/src/config/typeorm.ts
@@ -1,6 +1,5 @@
 import { DataSource, DataSourceOptions } from "typeorm";
 import {config as dotenvConfig} from 'dotenv';
-import { dirname } from "path";
 import { registerAs } from "@nestjs/config";
 
 dotenvConfig({path:'.env.development'});
@@ -9,7 +8,7 @@ const config = {
     type: 'postgres',
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    port: parseInt(process.env.DB_PORT, 10),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     entities:[__dirname + '/../**/*.entity{.ts,.js}'],
@@ -17,7 +16,7 @@ const config = {
     ssl: {
         rejectUnauthorized: false,  // A veces se puede necesitar para ciertas configuraciones de PostgreSQL
     },
-    synchronize: false, // TRUE
+    synchronize: true, // TRUE
     logging: false,
     dropSchema: false,
 }
