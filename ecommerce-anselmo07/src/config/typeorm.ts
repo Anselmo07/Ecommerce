e816da1afe5646172b1d8a865ec9fd603f38af55
anselmo <anselmo.vecchio.jose@gmail.com>
2024-12-18 17:32:49 -0300

    Prueba

--- ecommerce-anselmo07/src/config/typeorm.ts
+++ ecommerce-anselmo07/src/config/typeorm.ts
@@ -9,7 +9,7 @@ const config = {
     type: 'postgres',
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT as unknown as number,
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     entities:[__dirname + '/../**/*.entity{.ts,.js}'],
